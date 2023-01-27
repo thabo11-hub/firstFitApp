90bf77ec6c73a615f80d18b958ad80156c566def
Thabo Sodi <thabosodi11@gmail.com>
2023-01-27 17:50:27 +0200

    use normal view

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, Text } from 'react-native';
+import { View, Text } from 'react-native';
 
 export default function App() {
   return (
-    <SafeAreaView className='p-10 bg-red-500'>
+    <View className='flex-1 items-center justify-center bg-red-500'>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
-    </SafeAreaView>
+    </View>
   );
 }
