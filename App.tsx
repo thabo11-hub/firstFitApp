6d5fe3b75cbaab720c90e907f48b4952123ece77
Thabo Sodi <thabosodi11@gmail.com>
2023-01-27 22:09:45 +0200

    install navgation stack and rap content into container

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { View, Text } from 'react-native';
+import { SafeAreaView, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View className='flex items-center justify-center bg-slate-500'>
-      <Text className='text-red-500'>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <SafeAreaView className="flex-1 items-center justify-center bg-slate-500">
+        <Text className='text-red-500'>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    </NavigationContainer>
   );
 }
--- README.md
+++ README.md
@@ -11,3 +11,4 @@ npx expo install react-native-purchases
 
 yarn add @react-navigation/native
 npx expo install react-native-screens react-native-safe-area-context
+yarn add @react-navigation/native-stack
