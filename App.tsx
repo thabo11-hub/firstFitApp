05b27a10858172e0c7d2f5f6cbf1e2d4e8edb688
Thabo Sodi <thabosodi11@gmail.com>
2023-01-27 22:19:13 +0200

    Home screen

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreens from './screens/HomeScreens';
 
 const Stack = createNativeStackNavigator();
 
@@ -9,7 +10,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name='Home'  component={HomeScreen}/>
+        <Stack.Screen name='Home'  component={HomeScreens}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
