a69334a9c86edc51d6accca51506a9d9243f3e5a
Thabo Sodi <thabosodi11@gmail.com>
2023-02-13 10:16:05 +0200

    format

--- App.tsx
+++ App.tsx
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name='Home'  component={HomeScreens}/>
+        <Stack.Screen name='Home' component={HomeScreens}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
