1525c637e92857f1cd29a013130a6d852ea8022d
Thabo Sodi <thabosodi11@gmail.com>
2023-02-20 13:28:33 +0200

    back arrow

--- screens/DemoScreens.tsx
+++ screens/DemoScreens.tsx
@@ -1,6 +1,7 @@
 import { useNavigation } from '@react-navigation/native'
 import { NativeStackNavigationProp } from '@react-navigation/native-stack'
-import { View, Text, SafeAreaView } from 'react-native'
+import { Ionicons } from '@expo/vector-icons';
+import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
 import { RootStackParamList } from '../App'
 
 export type NavigationProp = NativeStackNavigationProp<RootStackParamList, "Demo">;
@@ -10,9 +11,13 @@ const DemoScreens = () => {
 
   return (
     <SafeAreaView>
+      <TouchableOpacity>
+        <Ionicons name="arrow-back" size={50} color="white" />
+        <Text>Go Back</Text>
+      </TouchableOpacity>
       <Text>DemoScreens</Text>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default DemoScreens
+export default DemoScreens;
