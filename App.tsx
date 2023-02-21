3cbaa14e9d413c315f8baf64e7709b66cdb58d91
Thabo Sodi <thabosodi11@gmail.com>
2023-02-21 16:11:02 +0200

    Payroll component

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreens from './screens/HomeScreens';
 import DemoScreens from './screens/DemoScreens';
+import Paywall from './screens/Paywall';
 
-export type RootStackParamList ={
+export type RootStackParamList = {
   Home: undefined;
   Paywall: undefined;
   Demo: undefined;
@@ -17,11 +16,11 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen options={{
-          headerShown: false
-        }} name='Home' component={HomeScreens} />
+        <Stack.Screen options={{ headerShown: false }} name='Home' component={HomeScreens} />
 
-        <Stack.Screen options={{ headerShown : false }} name='Demo' component={DemoScreens} />
+        <Stack.Screen options={{ headerShown: false }} name='Demo' component={DemoScreens} />
+
+        <Stack.Screen options={{ headerShown: false }} name='Paywall' component={Paywall} />
 
       </Stack.Navigator>
     </NavigationContainer>
---
+++ screens/Paywall.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Paywall = () => {
+  return (
+    <View>
+      <Text>Paywall</Text>
+    </View>
+  )
+}
+
+export default Paywall
